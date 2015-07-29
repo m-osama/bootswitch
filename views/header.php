981f7f37bb3a1aa8ebafb7d9fa98d8a180cdf3f5
Mohammed Osama <mohammedosama@gmail.com>
2015-07-29 15:50:40 +0200

    1st commet

---
+++ views/header.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Amr|Osama</title>
+
+    <!-- Mobile support -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Material Design for Bootstrap -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css">
+    <!-- <link href="dist/css/bootswatch.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/roboto.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/material-fullpalette.min.css" rel="stylesheet"> -->
+    <!-- <link href="dist/css/ripples.min.css" rel="stylesheet"> -->
+    <script type="text/javascript" async="" src="https://ssl.google-analytics.com/ga.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Dropdown.js -->
+    <link href="//cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.css" rel="stylesheet">
+
+  </head>
+ 
+<body>
+ <!-- inner style start here-->
+    <style nonce="z2c/KUU1H7aIEDkZL8YYnN1Z/i4">
+        
+        .text1 {
+          color: #ffffff;
+        }
+        .text2 {
+          color: #7f8c8d;
+        }
+        .text3 {
+              color: #424242;
+              font-size: 20px;
+              font-weight: 400;
+              line-height: 28px;
+        }
+        .text4 {
+              color: #FFF3E0;
+              /*font-size: 24px;*/
+        }
+        .color1 {
+          color: #FCE4EC;
+          background-color: #FF8A80;
+        }
+        .color2 {
+          color: #FCE4EC;
+          background-color: #009688;
+        }
+        .color3 {
+          color: #FCE4EC;
+          background-color: #9C27B0;
+        }
+        .colorfooter {
+          color: #2196F3;
+          background-color: #F9F9F9;
+          margin-bottom: 0px;
+        }
+        .bgcolor1 {
+          color: #FCE4EC;
+          background-color: #B39DDB;
+        }
+        
+      </style>
+      <!-- inner style end here -->
+
+	<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
