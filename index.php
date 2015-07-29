319c30aafb9020ea26eec2de730cd2e4da55a94b
Mohammed Osama <mohammedosama@gmail.com>
2015-07-29 16:26:01 +0200

    update 2

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ include 'views/header.php';
 switch( $_GET['page'] ) {
 	case 'themes':
 		include 'views/themes.php';
-		}
+		
 		break;
 	case 'upload':
 		include 'views/update.php';
-		}
+		
 		break;
 	default:
 		include 'views/home.php';
