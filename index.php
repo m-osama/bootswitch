784e47edc7afa33ab1f6b824f924a53f7ecd022f
Mohammed Osama <mohammedosama@gmail.com>
2015-07-29 15:59:36 +0200

    new update

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
-// Application-global configuration
-// include 'config/db.php';
-// Add models
-// include 'models/Employee.php';
-// include 'models/PdfReader.php';
-// $class_employee = new Employee();
+
 include 'views/header.php';
 switch( $_GET['page'] ) {
-	case 'list':
-		$employees = $class_employee->getAll();
-		include 'views/list.php';
-		break;
-	case 'view':
-		$id = $_GET['id'];
-		$employee = $class_employee->getOne( $id );
-		include 'views/view.php';
+	case 'themes':
+		include 'views/themes.php';
+		}
 		break;
 	case 'upload':
 		include 'views/update.php';
